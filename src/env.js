01d8405fa5c66f3f5651518fdceeebeaf1128727
Ali <105012194+aliysnm@users.noreply.github.com>
2026-02-23 08:45:16 +0300

    Deleted useless folder(s)

--- src/env.js
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyApBwi6CQy0sc65lzY9ijqWxN_B1MdJY5g",
-  authDomain: "pasa-account.firebaseapp.com",
-  projectId: "pasa-account",
-  storageBucket: "pasa-account.firebasestorage.app",
-  messagingSenderId: "461654215973",
-  appId: "1:461654215973:web:c7f3fe86a44dc2b79e9ecc"
-}
